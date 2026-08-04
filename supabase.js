f16a97f7edf5d62a8daf52da01bde7160df794ca
DuckmoreDuck <sesamomupets@gmail.com>
2026-08-03 23:46:21 -0300

    Refactor Supabase client initialization code

--- supabase.js
+++ supabase.js
@@ -95,6 +95,6 @@
 
 // 🟢 CONFIGURAÇÃO E INICIALIZAÇÃO DA SUA CONTA SUPABASE
 const SUPABASE_URL = 'https://bwrbduzlcbfbsrrnowam.supabase.co';
-const SUPABASE_KEY = 'SUA_CHAVE_ANON_AQUI'; // Coloque sua chave anon do Supabase aqui se ainda não colocou!
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3cmJkdXpsY2JmYnNycm5vd2FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU3NjYwMDgsImV4cCI6MjEwMTM0MjAwOH0.APLWUjOFPUFoqZ-_DMfjpFlo0xCw2W_drBjA_8EDrQw'; // Coloque sua chave anon do Supabase aqui se ainda não colocou!
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
